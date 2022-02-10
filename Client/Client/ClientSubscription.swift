8045178e04f89b1d25603dbadee2c491f3d2d6fb
ivan <kulikovia.72@icloud.com>
2022-02-10 15:01:30 +0200

    add two months subscription

--- Client/Client/ClientSubscription.swift
+++ Client/Client/ClientSubscription.swift
@@ -3,5 +3,6 @@ import Store
 enum ClientSubscription: String, CaseIterable, StoreProduct {
     case week = "com.kulikovia.client.store.week"
     case month = "com.kulikovia.client.store.month"
+    case twoMonths = "com.kulikovia.client.store.two_months"
     case year = "com.kulikovia.client.store.year"
 }
