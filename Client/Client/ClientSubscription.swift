c8c4de91d59ae121b95a32748c4056afdfaf5ef6
ivan <kulikovia.72@icloud.com>
2022-02-10 14:27:36 +0200

    add ClientSubscription

---
+++ Client/Client/ClientSubscription.swift
@@ -0,0 +1,5 @@
+enum ClientSubscription: String {
+    case week = "com.kulikovia.client.store.week"
+    case month = "com.kulikovia.client.store.month"
+    case year = "com.kulikovia.client.store.year"
+}
